54892c609b6ad5dfe88c571316e0855da4e21f54
akar0903 <104198301+akar0903@users.noreply.github.com>
2023-03-21 23:29:02 +0530

    Create Arranging cons.java

---
+++ Arranging cons.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        long k = (long)Math.sqrt(2*(long)n);
+        long sum = k*(k+1)/2;
+        if(sum>n) return (int)k-1;
+        return (int)k;
+    }
+}
