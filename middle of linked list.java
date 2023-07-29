f37df4cf5813778ac2cf0fdb118b314a521acf2f
akar0903 <104198301+akar0903@users.noreply.github.com>
2023-07-29 11:15:25 +0530

    middle of linked list.java

---
+++ middle of linked list.java
@@ -0,0 +1,12 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode a_pointer = head;
+        ListNode b_pointer = head;
+        while(b_pointer != null && b_pointer.next != null){
+            a_pointer = a_pointer.next;
+            b_pointer = b_pointer.next.next; 
+        }
+        return a_pointer;
+        
+    }
+}
