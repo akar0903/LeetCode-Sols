2c0427a6ebdc301f8bfef37ff32b3243017fca1e
akar0903 <104198301+akar0903@users.noreply.github.com>
2024-12-28 21:54:52 +0530

    Create 55. Jump Game.cs

---
+++ 55. Jump Game.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool CanJump(int[] nums) {
+        int finishIndex = nums.Length-1;
+        for(int i=nums.Length-1;i>=0;i--){
+            if(nums[i] + i >= finishIndex){
+                if(i == 0) return true;
+                finishIndex = i;
+            }
+        }
+        return false;
+    }
+}
