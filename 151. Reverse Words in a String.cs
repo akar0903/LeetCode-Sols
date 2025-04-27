89b9d49bf1f1f322cb332e3a2747a9071112a37e
akar0903 <104198301+akar0903@users.noreply.github.com>
2025-04-27 10:03:34 +0530

    Create 151. Reverse Words in a String.cs

---
+++ 151. Reverse Words in a String.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public string ReverseWords(string s) {
+        s = s.Trim();
+        string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        Array.Reverse(words);
+        return string.Join(" ", words);
+        
+    }
+}
